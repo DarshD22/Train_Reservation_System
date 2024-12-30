6403ea814ab8fd1390984cfcf7c47693d0187c9d
DarshD22 <dujrai.darsh0122@gmail.com>
2024-12-30 20:46:37 +0530

    updated next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
           return [
             {
               source: '/api/:path*',
-              destination: 'http://localhost:5000/api/:path*',  // Proxy to backend
+              destination: 'https://train-reservation-system-yob2.onrender.com/api/:path*',  // Proxy to backend
             },
           ];
         },
