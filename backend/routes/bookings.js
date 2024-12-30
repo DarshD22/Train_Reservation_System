dc359027fdfd27975ed28d7d39582e9e2606037c
DarshD22 <dujrai.darsh0122@gmail.com>
2024-12-30 16:07:56 +0530

    Remove all files from repository but keep them locally

--- backend/routes/bookings.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { reserveSeats, resetBookings ,getSeats} from '../controllers/bookingController.js';
-const router = express.Router();
-
-router.post('/reserve', reserveSeats);
-router.post('/reset', resetBookings);
-router.get('/seats', getSeats)
-
-export default router;
--- frontend/src/pages/bookings/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SeatBooking from './reserve.js'; // Importing the `reserve` component
-
-const BookingsPage = () => {
-  return <SeatBooking />;
-};
-
-export default BookingsPage;
